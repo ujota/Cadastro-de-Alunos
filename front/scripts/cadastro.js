73e1a30fe8700986e65d3afa1b52f5496a4f908b
ujota <joao.silveerio@gmail.com>
2022-11-02 12:25:24 -0300

    update-entrega

--- front/scripts/cadastro.js
+++ front/scripts/cadastro.js
@@ -9,14 +9,12 @@ formulario.addEventListener("submit", function(event){
     const email = document.querySelector("#inputEmail");
     const telefone = document.querySelector("#inputTelefone");
     const cpf = document.querySelector("#inputCpf");
-    const data_nascimento = document.querySelector("#inputDataNascimento");
 
 	
     const nome_cliente = nome.value;
     const email_cliente = email.value;
     const telefone_cliente = telefone.value;
     const cpf_cliente = cpf.value;
-    const data_cliente = data_nascimento.value;
     
     fetch('http://localhost:8080/aluno',{
         method: 'POST',
@@ -28,7 +26,6 @@ formulario.addEventListener("submit", function(event){
             "email" : email_cliente,
             "telefone": telefone_cliente,
             "cpf" : cpf_cliente,
-            "dataNascimento" : data_cliente,
         })
     })
     .then(querySet =>{
